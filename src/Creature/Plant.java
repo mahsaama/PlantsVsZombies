67343e84722e0aea531306b283e0e04a3d260e60
mahsaamani <mahsa.ama1391@gmail.com>
2019-12-25 00:08:19 +0330

    changes in attack and json

--- src/Creature/Plant.java
+++ src/Creature/Plant.java
@@ -1,4 +1,137 @@
 package Creature;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.util.ArrayList;
+
 public class Plant {
+    String name;
+    String bulletType;
+    private int id;
+    private int life;
+    private int rest;
+    private int numberOfSuns;
+    private int hurtOfBullet;
+    private int turn;
+    private int shootNum;
+    private int hurtToZombie;
+    private int zombieStopTurns;
+    private int offTurns;
+    private int minDistanceToShoot;
+    private int effectOnZombieSpeed;
+    private int turnsUnderEffect;
+    private int turnOfFront;
+    private int shootNumOfFront;
+    private int turnOfBack;
+    private int shootNumOfBack;
+    private int sunProduct;
+
+    public void setLife() {
+        this.life--;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBulletType() {
+        return bulletType;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getLife() {
+        return life;
+    }
+
+    public int getRest() {
+        return rest;
+    }
+
+    public int getNumberOfSuns() {
+        return numberOfSuns;
+    }
+
+    public int getHurtOfBullet() {
+        return hurtOfBullet;
+    }
+
+    public int getTurn() {
+        return turn;
+    }
+
+    public int getShootNum() {
+        return shootNum;
+    }
+
+    public int getHurtToZombie() {
+        return hurtToZombie;
+    }
+
+    public int getZombieStopTurns() {
+        return zombieStopTurns;
+    }
+
+    public int getOffTurns() {
+        return offTurns;
+    }
+
+    public int getMinDistanceToShoot() {
+        return minDistanceToShoot;
+    }
+
+    public int getEffectOnZombieSpeed() {
+        return effectOnZombieSpeed;
+    }
+
+    public int getTurnsUnderEffect() {
+        return turnsUnderEffect;
+    }
+
+    public int getTurnOfFront() {
+        return turnOfFront;
+    }
+
+    public int getShootNumOfFront() {
+        return shootNumOfFront;
+    }
+
+    public int getTurnOfBack() {
+        return turnOfBack;
+    }
+
+    public int getShootNumOfBack() {
+        return shootNumOfBack;
+    }
+
+    public int getSunProduct() {
+        return sunProduct;
+    }
+
+
+
+    public void attack(Zombie zombie,boolean sameCell){
+        if (sameCell){
+            if(zombie.getName().equals("FootballZombie") && name == "Cactus"){
+                if(turn % 2 == 0){
+                    zombie.setLife();
+                    if (zombie.getLife() == 0){
+                        zombie = null;
+                        //TODO
+                    }
+                }
+            }
+        }
+    }
+    public void checkPlantsInRow(ArrayList<Plant> plants){
+
+    }
+    public void checkZombiesInRow(ArrayList<Zombie> zombies){
+
+    }
 }
--- src/Creature/Zombie.java
+++ src/Creature/Zombie.java
@@ -1,4 +1,84 @@
 package Creature;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.util.ArrayList;
+
 public class Zombie {
+    private String name;
+    private int id;
+    private int life;
+    private int speed;
+    private boolean hat;
+    private int lifeDec;
+    private int remainTurns;
+    private int guardLife;
+    private boolean guard;
+    private boolean car;
+
+    public void setLife() {
+        this.life--;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getLife() {
+        return life;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public boolean isHat() {
+        return hat;
+    }
+
+    public int getLifeDec() {
+        return lifeDec;
+    }
+
+    public int getRemainTurns() {
+        return remainTurns;
+    }
+
+    public int getGuardLife() {
+        return guardLife;
+    }
+
+    public boolean isGuard() {
+        return guard;
+    }
+
+    public boolean isCar() {
+        return car;
+    }
+
+    public void attack(Plant plant){
+
+        if (plant.getName().equals("CherryBomb") || plant.getName().equals("Jalapeno")
+        || name == "PogoZombie"){
+            return;
+        }
+        if (plant.getLife() > 0){
+            plant.setLife();
+            if (plant.getLife() == 0){
+                plant = null;
+                //TODO
+            }
+        }
+        if (car){
+
+        }
+    }
+
 }
