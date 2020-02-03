f71ecd95cb18779c296b5b9f2061ec0401199231
Ludovimeh <najafi.mehraneh@gmail.com>
2020-02-04 01:56:47 +0330

    doing Zombie mode by mehraneh

--- src/Menu/ZombiePlayView.java
+++ src/Menu/ZombiePlayView.java
@@ -2,6 +2,8 @@ package Menu;
 
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.effect.GaussianBlur;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -13,6 +15,15 @@ public class ZombiePlayView implements GameModeView{
     private GaussianBlur blur = new GaussianBlur();
     private Image backImage = new Image("pics/frontyard.png");
     private ImageView backImageView = new ImageView(backImage);
+    private Button menuButton = new Button();
+    private Button buyButton = new Button();
+
+    private Image menuImage = new Image("pics/menu.png");
+    private Image buyImage = new Image("pics/buyZombie.png");
+
+    private ImageView menuImageView = new ImageView(menuImage);
+    private ImageView buyImageView = new ImageView(buyImage);
+
 
 
     private int height = 700;
@@ -24,6 +35,17 @@ public class ZombiePlayView implements GameModeView{
         backImageView.setFitWidth(width);
         zombiePlayScene = new Scene(zombiePlayRoot, width, height);
         zombiePlayRoot.getChildren().add(backImageView);
+
+        menuImageView.relocate(1100,650);
+        menuButton.relocate(1100, 650);
+        buyImageView.relocate(1100, 50);
+        buyButton.relocate(1100, 50);
+        zombiePlayRoot.getChildren().add(menuImageView);
+        zombiePlayRoot.getChildren().add(menuButton);
+        zombiePlayRoot.getChildren().add(buyImageView);
+        zombiePlayRoot.getChildren().add(buyButton);
+
+
         attack();
 
     }
