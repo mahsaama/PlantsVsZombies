99c354c4335f6cf349b3e03624e4c1aea99c9666
mahsa.ama <mahsa.ama1391@gmail.com>
2020-02-09 14:22:35 +0330

    rail game

--- src/Game/RailModeOfGame.java
+++ src/Game/RailModeOfGame.java
@@ -1,14 +1,19 @@
 package Game;
 
+import Creature.Plant;
+import Shop.Shop;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 
 import java.awt.*;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 
-public class RailModeOfGame {
+public class RailModeOfGame extends Game{
     private Group railPlayRoot;
     private Scene railPlayScene;
 
@@ -25,7 +30,23 @@ public class RailModeOfGame {
         bgImageView.setFitWidth(width);
         railPlayScene = new Scene(railPlayRoot, width, height);
         railPlayRoot.getChildren().add(bgImageView);
+        int zombieNextTurn = 0;
+        ArrayList<Plant> cards = new ArrayList<> ( );
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                SecureRandom rand = new SecureRandom ( );
+                int plantNextTurn = 0;
+                if (turn == plantNextTurn) {
+                    Image image = new Image(randomCard(cards));
+                    int randomNum = rand.nextInt (3) + 2;
+                    plantNextTurn = turn + randomNum;
+                    ImageView imageView = new ImageView(image);
 
+
+                }
+            }
+        }).start();
 
     }
 }
