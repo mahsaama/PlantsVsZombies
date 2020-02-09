b78087a273cc60588c727aa9b6d01d3d6f667eda
mahsa.ama <mahsa.ama1391@gmail.com>
2020-02-09 13:05:28 +0330

    debug

--- src/Game/ZombieModeOfGame.java
+++ src/Game/ZombieModeOfGame.java
@@ -94,7 +94,7 @@ public class ZombieModeOfGame {
         zombiePlayRoot.getChildren().add(buyButton);
         zombiePlayRoot.getChildren().add(ok);
 
-        
+
 
         checkMovements();
     }
